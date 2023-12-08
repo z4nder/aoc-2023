af7e6e76ae43e85520b91a724da4ae0e78f8a902
z4nder <gusttavo212@gmail.com>
2023-12-07 21:40:42 -0300

    Finish day 2 part 02

--- cube-conudrum-02/src/main.rs
+++ cube-conudrum-02/src/main.rs
@@ -64,6 +64,10 @@ impl GameRecord {
         GameRecord { value }
     }
 
+    fn create_from_hash_map(value: HashMap<CubeColors, i32>) -> GameRecord {
+        GameRecord { value }
+    }
+
     fn create_records(records: Vec<&str>) -> Vec<GameRecord> {
         let mut game_records: Vec<GameRecord> = Vec::new();
 
@@ -101,13 +105,64 @@ fn main() {
     let file = read_file("./inputs/input.txt");
     let games = parse_games(file);
 
-    let mut cubes_quantity = HashMap::new();
+    // let mut cubes_quantity = HashMap::new();
 
-    cubes_quantity.insert(CubeColors::RED, 12);
-    cubes_quantity.insert(CubeColors::GREEN, 13);
-    cubes_quantity.insert(CubeColors::BLUE, 14);
+    // cubes_quantity.insert(CubeColors::RED, 12);
+    // cubes_quantity.insert(CubeColors::GREEN, 13);
+    // cubes_quantity.insert(CubeColors::BLUE, 14);
 
-    println!("Result: {:?}", sum_valid_ids(games, cubes_quantity));
+    // println!("Result: {:?}", sum_valid_ids(games, cubes_quantity));
+
+    println!("Result: {:?}", get_power_of_games(games));
+}
+
+fn get_power_of_games(games: Vec<Game>) -> i32 {
+    let mut tota_power = 0;
+
+    for game in games {
+        let game_record = get_min_count_color_game_record(game);
+        let game_power = get_power_of_game_record(game_record);
+        tota_power += game_power;
+    }
+
+    tota_power
+}
+
+fn get_power_of_game_record(game_record: GameRecord) -> i32 {
+    game_record.value.get(&CubeColors::RED).unwrap()
+        * game_record.value.get(&CubeColors::GREEN).unwrap()
+        * game_record.value.get(&CubeColors::BLUE).unwrap()
+}
+
+fn get_min_count_color_game_record(game: Game) -> GameRecord {
+    let mut max_cube_value = HashMap::new();
+
+    max_cube_value.insert(CubeColors::RED, 0);
+    max_cube_value.insert(CubeColors::GREEN, 0);
+    max_cube_value.insert(CubeColors::BLUE, 0);
+
+    for game_record in game.records {
+        if game_record.value.get(&CubeColors::RED) > max_cube_value.get(&CubeColors::RED) {
+            max_cube_value.insert(
+                CubeColors::RED,
+                game_record.value.get(&CubeColors::RED).unwrap().clone(),
+            );
+        }
+        if game_record.value.get(&CubeColors::GREEN) > max_cube_value.get(&CubeColors::GREEN) {
+            max_cube_value.insert(
+                CubeColors::GREEN,
+                game_record.value.get(&CubeColors::GREEN).unwrap().clone(),
+            );
+        }
+        if game_record.value.get(&CubeColors::BLUE) > max_cube_value.get(&CubeColors::BLUE) {
+            max_cube_value.insert(
+                CubeColors::BLUE,
+                game_record.value.get(&CubeColors::BLUE).unwrap().clone(),
+            );
+        }
+    }
+
+    GameRecord::create_from_hash_map(max_cube_value)
 }
 
 fn sum_valid_ids(games: Vec<Game>, cubes_quantity: HashMap<CubeColors, i32>) -> i32 {
@@ -175,7 +230,10 @@ fn read_file(file_path: &str) -> String {
 
 #[cfg(test)]
 mod tests {
-    use crate::{parse_games, read_file, Game, GameRecord};
+    use crate::{
+        get_min_count_color_game_record, get_power_of_games, parse_games, read_file, Game,
+        GameRecord,
+    };
 
     #[test]
     fn test_parse_games() {
@@ -191,5 +249,37 @@ mod tests {
         let games = parse_games(file);
 
         assert_eq!(games[0], expected_game);
+    }
+
+    #[test]
+    fn test_get_min_count_color_game_record() {
+        let records = vec![
+            GameRecord::create(4, 0, 3),
+            GameRecord::create(1, 2, 6),
+            GameRecord::create(0, 2, 0),
+        ];
+
+        let game = Game::create(1, records);
+
+        let game_record = get_min_count_color_game_record(game);
+
+        let expecte_game_record = GameRecord::create(4, 2, 6);
+
+        assert_eq!(game_record, expecte_game_record);
+    }
+
+    #[test]
+    fn test_get_power_of_games() {
+        let records = vec![
+            GameRecord::create(4, 0, 3),
+            GameRecord::create(1, 2, 6),
+            GameRecord::create(0, 2, 0),
+        ];
+
+        let game = Game::create(1, records);
+
+        let power = get_power_of_games(vec![game]);
+
+        assert_eq!(power, 48);
     }
 }
